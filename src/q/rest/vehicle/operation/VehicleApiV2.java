55dbdb2265d940d532858c956d078b0aeba79c4b
fareed <fareed@qetaa.com>
2019-01-31 12:39:59 +0300

    added vehicles makes to public api

--- src/q/rest/vehicle/operation/VehicleApiV2.java
+++ src/q/rest/vehicle/operation/VehicleApiV2.java
@@ -2,14 +2,15 @@ package q.rest.vehicle.operation;
 
 import q.rest.vehicle.dao.DAO;
 import q.rest.vehicle.filter.ValidApp;
+import q.rest.vehicle.model.contract.PublicMake;
+import q.rest.vehicle.model.contract.PublicModel;
+import q.rest.vehicle.model.contract.PublicModelYear;
 import q.rest.vehicle.model.contract.PublicModelYearContained;
+import q.rest.vehicle.model.entity.Make;
 import q.rest.vehicle.model.entity.ModelYear;
 
 import javax.ejb.EJB;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.ArrayList;
@@ -48,7 +49,27 @@ public class VehicleApiV2 {
     }
 
 
-
-
+//    @ValidApp
+    @GET
+    @Path("makes")
+    public Response getAllVehicles() {
+        try {
+            String sql = "select b from PublicMake b where b.status = :value0 order by b.name";
+            List<PublicMake> makes = dao.getJPQLParams(PublicMake.class, sql, 'A');
+            for(PublicMake publicMake : makes){
+                sql = "select b from PublicModel b where b.makeId = :value0 and b.status =:value1 order by b.name";
+                List<PublicModel> models = dao.getJPQLParams(PublicModel.class, sql, publicMake.getId(), 'A');
+                publicMake.setModels(models);
+                for(PublicModel model: models){
+                    sql = "select b from PublicModelYear b where b.modelId = :value0 and b.status =:value1 order by b.year";
+                    List<PublicModelYear> modelYears = dao.getJPQLParams(PublicModelYear.class, sql, model.getId(), 'A');
+                    model.setModelYears(modelYears);
+                }
+            }
+            return Response.status(200).entity(makes).build();
+        } catch (Exception ex) {
+            return Response.status(500).build();
+        }
+    }
 
 }
